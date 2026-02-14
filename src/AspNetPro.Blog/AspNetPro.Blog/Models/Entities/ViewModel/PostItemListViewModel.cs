84e442a8600e61bebf03318989f0808ee7780538
Rodrigo Morello <polo.opeo@hotmail.com>
2026-02-14 19:11:09 -0300

    #6 Listagem e visualização de posts

--- src/AspNetPro.Blog/AspNetPro.Blog/Models/Entities/ViewModel/PostItemListViewModel.cs
+++ src/AspNetPro.Blog/AspNetPro.Blog/Models/Entities/ViewModel/PostItemListViewModel.cs
@@ -5,6 +5,7 @@
         public int PostId { get; set; }
         public string Title { get; set; }
         public string Summary { get; set; }
+        public string Content { get; set; }
         public string PublishedOn { get; set; }
     }
 }
--- src/AspNetPro.Blog/AspNetPro.Blog/Pages/Posts/Index.cshtml.cs
+++ src/AspNetPro.Blog/AspNetPro.Blog/Pages/Posts/Index.cshtml.cs
@@ -9,9 +9,20 @@ namespace AspNetPro.Blog.Pages.Posts;
 public class IndexModel(BlogContext blogContext) : PageModel
 {
     public IList<PostItemListViewModel> Posts { get; set; }
-    public void OnGet()
+    public void OnGet(string q)
     {
-        this.Posts = blogContext.Posts
+        IQueryable<Post> posts = blogContext.Posts;
+
+        if (!String.IsNullOrWhiteSpace(q))
+        {
+            posts = posts.Where(post =>
+                post.Title.Contains(q) ||
+                post.Summary.Contains(q) ||
+                post.Content.Contains(q)
+            );
+        }
+
+        Posts = posts
             .OrderByDescending(x => x.PublishedOn)
             .Select(x => new PostItemListViewModel
             {
